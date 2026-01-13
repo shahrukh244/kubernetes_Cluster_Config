4f33dfe179f5abe2882fa05af7600e33253df762
shahrukh244 <163628121+shahrukh244@users.noreply.github.com>
2026-01-13 21:24:59 +0530

    Update local-run_all.py

--- service_Node_Config/service_Node-01/scripts/local-run_all.py
+++ service_Node_Config/service_Node-01/scripts/local-run_all.py
@@ -23,10 +23,7 @@ SCRIPTS = [
     "12-bind9.yaml",
     "13-ntp.yaml",
     "14-haproxy.yaml",
-    "15-drbd.yaml",
-    "15.1-drbd-disk-moniter.py",
-    "15.2-drbd-exports.yaml",
-    "16-reboot.yaml",
+    "15-reboot.yaml",
 ]
 
 def log_message(msg):
